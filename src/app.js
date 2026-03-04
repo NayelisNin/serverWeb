90a7c9b529811ac5ab6639704690cbbcc28d90b4
Nayelis Nin <nayelisninb.12@gmail.com>
2026-03-04 09:46:51 -0400

    mas cosas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const port = process.env.PORT || 3000;
+
+console.log("__dirname:", __dirname);
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Hello, 6to A Informatica!');
+    res.render("index");
 });
 
 app.listen(port, () => {
